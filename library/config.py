e5139008ef181e5191cb4e87cd4174214f5a4b61
Hoàng Mạnh Thắng <21521428@gm.uit.edu.vn>
2024-03-20 15:39:15 +0700

    Cập nhật URL database

--- library/config.py
+++ library/config.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-SECRET_KEY = os.getenv('KEY')
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@localhost/healthyapp"
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
