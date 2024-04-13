46ba068337b56396a58c9f98cc88875400116aba
Hoàng Mạnh Thắng <21521428@gm.uit.edu.vn>
2024-04-14 01:43:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ app = create_app()
 if __name__ == '__main__':
 	(app.run(debug=True))
 	# 121b81dc55849fc9f81a5f7dabe42b25680fc0afdb1a4e2134ce52c935595d4c
+
+
+#ngrok http --domain=premium-singularly-meerkat.ngrok-free.app 5000
